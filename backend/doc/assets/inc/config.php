785c5bf9c6354130eda28710ef0dabba1afd2622
Rimah Houssameldine <rimahhd3@gamil.com>
2024-02-20 20:35:09 +0200

    ..

--- backend/doc/assets/inc/config.php
+++ backend/doc/assets/inc/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="admin";
-$dbpass="";
-$servername="localhost";
+$dbuser="gdavmfzwyf@%";
+$dbpass="*963836664285921EE9175CFAE187428706C877FC";
+$servername="%";
 $db="gdavmfzwyf";
 $mysqli=new mysqli($servername,$dbuser, $dbpass, $db);
 ?>
